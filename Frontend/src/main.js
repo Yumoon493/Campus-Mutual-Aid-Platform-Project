9ba9f6fcc24ea6c5e0c6e60a34bcba53fda37857
Starfruit <2490151535@qq.com>
2026-03-08 18:13:51 +0800

    Configure Axios to send credentials with requests
    Set Axios to include credentials with requests.

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -6,7 +6,7 @@ import 'element-plus/dist/index.css'
 import i18n from './i18n' 
 import axios from 'axios' // 引入 axios
 
-// 【核心修复】告诉 Axios：以后无论去哪个页面发请求，都必须带上凭证！
+
 axios.defaults.withCredentials = true 
 
 const app = createApp(App)
